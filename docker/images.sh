88c13f5e4d80d8199914e90ab149e1f384c0fda6
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2020-09-27 16:46:56 +0000

    image path

--- docker/images.sh
+++ docker/images.sh
@@ -2,7 +2,31 @@
 #
 # List image directory
 
-if [ -z "$1" ]; then docker images; fi
+# -dir    display image dir; with the image id, print
+#         all directories in which image is located
+
+# without arguments display all the files related to iamge ID's
+# in turn:
+#   /var/lib/docker/image/overlay2/imagedb/content/sha256/4bb46517cac397bdb0bab6eba09b0e1f8e90ddd17cf99662997
+#
+# or with --no-trunc
+#   /var/lib/docker/image/overlay2/imagedb/content/sha256/4bb46
+#
+# or with --names
+#   /var/lib/docker/image/overlay2/imagedb/content/sha256/[nginx]
+
+
+# With test(1), if file readable... give more information about needed user
+# or group.
+[ $UID -eq 0 ]  || { echo "Run as root..."; exit 1; }
+
+# Construct the path
 info=( $( docker info | awk -F: '/(Docker Root Dir)|(Storage Driver)/ {print $2}' ) )
-path=${info[0]}/image/${info[1]}/imagedb/content/
-ls -l $path
+path=${info[1]}/image/${info[0]}/imagedb/ #content/
+
+# Here loop throuugh all the images or only those found on command line
+test -z "$1" || { find $path -name "*$1*" -exec file {} \; ; exit; }
+
+# add interactive
+#echo "Image path: '$path'"
+#sudo ls -l $path
