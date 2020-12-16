c24913e2aad80dab8e3abc6c9f9ef5a623c2db8c
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2020-12-16 09:38:47 +0100

    network settings

--- docker/network_settings.sh
+++ docker/network_settings.sh
@@ -4,3 +4,9 @@
 test $1 || { echo "`basename $0` CONTAINER_NAME_OR_ID"; exit 1; }
 
 docker inspect "$1" -f "{{json .NetworkSettings}}" | grep -o "\(NetworkID\|IPAddress\)[^,]\+" | sed -n '/NetworkID/,/IPAddress/ {s/"//; s/:/: /gp}'
+# here if you read container id, we can get all the containers 
+# attached to the network
+
+#docker inspect network NET_ID -f "{{json.Containers}}" |\
+  #grep -o '\(Name[^,]\+\)' |\
+  #sed 's/"//g'
