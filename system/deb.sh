9feeaeb9a09247f0a7e1cd556abcbbbafbbff52d
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-01-04 11:57:49 +0100

    Carbon work

--- system/deb.sh
+++ system/deb.sh
@@ -3,35 +3,42 @@
 # Find package that ownes a file
 
 usage() {
-  ret=${1:0}
+  ret=${1:-0}
   echo "Usage: `basename $0` FILE|EXE"
   echo "Print name of the package owing the file or executable"
   exit "$ret"
 }
-
-test -n ¨$1¨ || usage
+test -n "$1" || usage
 
 # If we just give the name to dpkg(1) it will search for regexp. 
 # We want to be more concrete limiting search to the file if it
 # doesn't have slashes so for owner of the executable.
-#
 if [  -e "$1"  ]
 then 
     file="$1"
+    # here if it is in the current directory == has no slashes, './' should
+    # be appended, otherwise will be treated as the pattern
 elif
-  ! [ "$1" =~ / ]   # if it was slash it would be already found as a file before
+  ! [[ "$1" =~ / ]]   # if it was slash it would be already found as a file before
   then
-    file=`which ¨$1¨`     # here if program was not found it will result no string
+    file=`which $1`     # here if program was not found it will result in no string
 fi
+
+# Now $file is set to be an absolute path, otherwise it will be treated by `dpkg -S' as 
+# a regexp and result in displaying all the files that match the pattern in the matched 
+# package names.
 
 if [ -z "$file" ]
 then
-  echo "error: \"$1\": no such file or executable"    # here proceed with find maybe
+  echo "error: \"$1\": no such file or executable"    
   exit 1
+  # here proceed with find maybe
 fi
 
-#owner=$( dpkg -S $file | cut -d : -f 1  )
-#echo "owner: $owner"
+owner=$( dpkg -S $file | cut -d : -f 1  )
+echo "owner: $owner"
+
+# -l   how I was parsing the options? waht were the methods?
 
 # Operate on a package owner
 #case $1 in
